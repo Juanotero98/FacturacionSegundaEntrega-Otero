2f0fdf32a0a3aa90f7e42faa7c86194aedacd95a
Juanotero98 <oterojuan1998@gmail.com>
2024-07-09 17:22:04 -0300

    entities

---
+++ src/test/java/com/commerce2/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce2.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
